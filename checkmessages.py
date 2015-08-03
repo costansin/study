4370cf74e43a470018cfafa500e08b6b92abb87d
costansin <costansinus@gmail.com>
2015-08-03 14:38:03 +0300

    version 2.0
    every 10 sec checks for new messages, appearing with Tkinter-window if
    there are some, so by non-appearing it ensures one, that there's nothing
    to check.
    A huge problem: now one CANNOT send a message without any unread
    messages inbox. For that use the previous version.

--- checkmessages.py
+++ checkmessages.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8
 import requests
 import time
+from tkinter import *
 #https://oauth.vk.com/authorize?client_id=5015702&scope=notify,friends,photos,audio,video,docs,notes,pages,status,offers,questions,wall,groups,messages,notifications,stats,ads,offline&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token
 sleepTime = 1
+waitTime = 10
 photosizes = [2560, 1280, 807, 604, 512, 352, 256, 130, 128, 100, 75, 64]
 token_file = open('token_file.txt', 'r') 
 token_list = [token[:-1] for token in token_file.readlines() if token[0]!='#'] #start line with # to make it comment
 token_file.close()
+printm=''
+width=0
+height=0
 
 def call_api(method, params, token):        
         params["access_token"] = token
@@ -39,16 +44,40 @@ def charfilter(s):
                         r+='vk.com/images/emoji/'+hex(ord(c)+3627804672).upper()[2:]+'_2x.png'
         return r
 
+def prints(s):
+        global printm
+        printm+=s+'\n'
+
+def print_attachments(attache, token):
+        if attache is not None:
+                for attached in attache:
+                        type = attached.get('type')
+                        stuff = attached.get(type)
+                        if (type=='photo')or(type=='sticker'):
+                                for size in photosizes:
+                                        link=stuff.get('photo_'+str(size))
+                                        if link is not None:
+                                                prints(link)
+                                                break
+                        elif type=='video':
+                                req = str(stuff.get('owner_id'))+'_'+str(stuff.get('id'))+'_'+str(stuff.get('access_key'))
+                                prints(call_api('video.get', {'videos': req}, token).get('items')[0].get('player'))
+                        elif type=='wall':
+                                prints('vk.com/wall'+str(stuff.get('to_id'))+'_'+str(stuff.get('to_id')))
+                        else:
+                                url = stuff.get('url')
+                                prints(url[:url.find('?extra')])
+
 def getHistory(N, uid, token):
         history = call_api('messages.getHistory', {'count': N, 'user_id': uid}, token).get('items')
         for message in reversed(history):
-                print(charfilter(message.get('body')))
+                prints(charfilter(message.get('body')))
                 print_attachments(message.get('attachments'), token)
                 fwd = message.get('fwd_messages')
                 if fwd is not None:
-                        print('fwd_messages')
+                        prints('fwd_messages')
                         for fwdm in fwd:
-                                print(str(fwdm.get('user_id'))+'#'+charfilter(fwdm.get('body')))
+                                prints(str(fwdm.get('user_id'))+'#'+charfilter(fwdm.get('body')))
 
 def messaging():
         print('messaging')
@@ -56,10 +85,9 @@ def messaging():
                 m = ''
                 s = ''
                 userid = 0
-                while (s=='')or(s[-1]!='#'):
+                while (s=='')or((s[-1]!='#')and(s[-1]!='№')):
                         s=input()
-                        if m=='':
-                                if s=='': return(0)
+                        if (m=='')&(s==''): return(0)
                         sharp = s.find('#')
                         if userid==0:
                                 try:
@@ -80,71 +108,70 @@ def messaging():
                         m+='\n'+s
                 m=m[:-1]
                 if userid==0: return(0)
+                global printm
                 if userid<0:
                          call_api('wall.post', {'owner_id': userid, 'from_group': 1, 'message': m}, token_list[token_num])
                 if m=='\n':
                         call_api('messages.markAsRead', {'peer_id': userid}, token_list[token_num])
                 elif m=='\n#':
                         getHistory(10, userid, token_list[token_num])
+                        print(printm)
                 else:
                         call_api('messages.send', {'user_id': userid, 'message': m}, token_list[token_num])
+                        getHistory(10, userid, token_list[token_num])
+                        print(printm)
                 print('Done')
 
-def print_attachments(attache, token):
-        if attache is not None:
-                for attached in attache:
-                        type = attached.get('type')
-                        stuff = attached.get(type)
-                        if (type=='photo')or(type=='sticker'):
-                                for size in photosizes:
-                                        link=stuff.get('photo_'+str(size))
-                                        if link is not None:
-                                                print(link)
-                                                break
-                        elif type=='video':
-                                req = str(stuff.get('owner_id'))+'_'+str(stuff.get('id'))+'_'+str(stuff.get('access_key'))
                                 print(call_api('video.get', {'videos': req}, token).get('items')[0].get('player'))
-                        elif type=='wall':
-                                print('vk.com/wall'+str(stuff.get('to_id'))+'_'+str(stuff.get('to_id')))
-                        else:
-                                url = stuff.get('url')
-                                print(url[:url.find('?extra')])
-
 def check_inbox():
+        A=0
         for mytoken in token_list:
                 myname = call_api('users.get', {}, mytoken)[0]
                 viewed_time = call_api('notifications.get',{'count': '0'}, mytoken).get('last_viewed')
                 notif_resp = call_api('notifications.get',{'start_time': viewed_time}, mytoken)
-                r = notif_resp.get('count')
                 resp = call_api('messages.getDialogs', {'unread': '1'}, mytoken)
-                print(myname.get('first_name')+' '+myname.get('last_name')+' - '+str(resp.get('count'))+' new dialogues'+' - '+str(r)+' new responses')
+                r = notif_resp.get('count')
+                t = resp.get('count')
+                A+=r+t
+                prints(myname.get('first_name')+' '+myname.get('last_name')+' - '+str(t)+' new dialogues'+' - '+str(r)+' new responses')
                 for x in resp.get('items'):
                         N = x.get('unread')
                         mes = x.get('message')
                         if mes.get('chat_id') is None: #check dialogue is not a chat
                                 uid = mes.get('user_id')
                                 respname = call_api('users.get', {'user_ids': uid}, mytoken)[0]
-                                print(respname.get('first_name')+' '+respname.get('last_name')+' '+str(uid)+' '+str(N)+' messages')
+                                prints(respname.get('first_name')+' '+respname.get('last_name')+' '+str(uid)+' '+str(N)+' messages')
                                 getHistory(N, uid, mytoken)
-                print("-------")
+                prints("-------")
                 for x in reversed(notif_resp.get('items')):
                         parent = x.get('parent')
                         parent_id = parent.get('to_id')
                         if parent_id is None: parent_id = parent.get('post').get('to_id')
-                        print('vk.com/wall'+str(parent_id)+'_'+str(parent.get('id')))
+                        prints('vk.com/wall'+str(parent_id)+'_'+str(parent.get('id')))
                         feedback = x.get('feedback')
                         comment = feedback.get('text')
                         if comment is None:
-                                print(x.get('type'))
+                                prints(x.get('type'))
                         else:
-                                print(charfilter(comment))
+                                prints(charfilter(comment))
                                 print_attachments(feedback.get('attachments'), mytoken)
-                print("_____________")
+                prints("_____________")
+        return(A) #messages+notifies of all tokens
 
 def main():
         while True:
-                check_inbox()
-                messaging()
+                global printm
+                printm=''
+                if check_inbox()>0:
+                        master=Tk()
+                        w = Canvas(master, width=width, height=height)
+                        w.pack()
+                        w = Message(master, text=printm)
+                        w.pack()
+                        master.mainloop()
+                        print(printm)
+                        messaging()
+                time.sleep(waitTime)
         
 if __name__ == '__main__':
         main()
